9802c70f95a2e7db902ef3ca2f619aff30fcf28f
Zenky <bzenky@gmail.com>
2022-04-04 20:57:45 -0300

    refactor: componentizing dashboard left cards

---
+++ components/DashLeftCards/index.tsx
@@ -0,0 +1,84 @@
+import Image from 'next/image'
+
+import {
+    DashboardCardTitle,
+    DashboardLeftCards,
+    DashboardMinorCard,
+    DashboardSpan,
+    PokemonAttributesCard,
+    PokemonAttributesGroup,
+    PokemonHWCard,
+    PokemonImgCard,
+    PokemonProperty,
+    PokemonTypeCard,
+} from '../../styles/styles'
+
+import PokeballGif from '../../public/pokeball.gif'
+
+type ParentProps = {
+    pokemon: {
+        loading: boolean
+        data: {
+            pokemon: {
+                height: number
+                weight: number
+                stats: any
+                types: any
+                sprites: {
+                    front_default: string
+                }
+            }
+        }
+    }
+}
+
+type Type = {
+    type: {
+        name: string
+    }
+}
+
+type Stat = {
+    base_stat: number
+    stat: { name: string }
+}
+
+export function DashLeftCards({ pokemon }: ParentProps) {
+    return (
+        <DashboardLeftCards>
+            <PokemonImgCard>
+                <Image src={pokemon.loading ? PokeballGif : pokemon.data.pokemon.sprites.front_default} width={370} height={370} />
+            </PokemonImgCard>
+            <PokemonTypeCard>
+                <DashboardCardTitle>Type</DashboardCardTitle>
+                {!pokemon.loading && pokemon.data.pokemon.types.map((type: Type) =>
+                    <PokemonProperty key={Math.random()} className={type.type.name}>
+                        {type.type.name}
+                    </PokemonProperty>
+                )}
+            </PokemonTypeCard>
+            <DashboardMinorCard>
+                <PokemonHWCard>
+                    <DashboardCardTitle>Height:</DashboardCardTitle>
+                    <DashboardSpan>
+                        {!pokemon.loading && (pokemon.data.pokemon.height / 10) + ' m'}
+                    </DashboardSpan>
+                </PokemonHWCard>
+                <PokemonHWCard>
+                    <DashboardCardTitle>Weight:</DashboardCardTitle>
+                    <DashboardSpan>{!pokemon.loading && (pokemon.data.pokemon.weight / 10) + ' kg'}</DashboardSpan>
+                </PokemonHWCard>
+            </DashboardMinorCard>
+            <PokemonAttributesCard>
+                <DashboardCardTitle>Attributes</DashboardCardTitle>
+                <PokemonAttributesGroup>
+                    {!pokemon.loading && pokemon.data.pokemon.stats.map((stat: Stat) =>
+                        <PokemonProperty key={Math.random()} className="attr">
+                            {stat.base_stat} {stat.stat.name}
+                        </PokemonProperty>
+                    )}
+                </PokemonAttributesGroup>
+            </PokemonAttributesCard>
+        </DashboardLeftCards>
+    )
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,32 +1,22 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
-import Image from 'next/image'
 import { useColorMode } from '@chakra-ui/react'
 import { gql, useQuery } from '@apollo/client';
 import {
     DashboardCards,
     DashboardCardTitle,
     DashboardContent,
-    DashboardLeftCards,
     DashboardMain,
-    DashboardMinorCard,
     DashboardRightCards,
-    DashboardSpan,
     Pokemon,
-    PokemonAttributesCard,
-    PokemonAttributesGroup,
     PokemonDescription,
     PokemonDescriptionCard,
     PokemonEvolutionCard,
-    PokemonHWCard,
-    PokemonImgCard,
-    PokemonProperty,
-    PokemonTypeCard,
 } from '../styles/styles'
 
-import PokeballGif from '../public/pokeball.gif'
 import { AsideNav } from '../components/AsideNav'
 import { DashHeader } from '../components/DashHeader';
+import { DashLeftCards } from '../components/DashLeftCards';
 
 const Dashboard: NextPage = () => {
     const { colorMode, toggleColorMode } = useColorMode()
@@ -87,7 +77,6 @@ const Dashboard: NextPage = () => {
 
     return (
         <DashboardContent>
-
             <AsideNav
                 pokemonName={pokemonName}
                 pokemonId={pokemonId}
@@ -98,7 +87,6 @@ const Dashboard: NextPage = () => {
             />
 
             <DashboardMain bg={colorMode == 'light' ? '#00b4eb' : '#0d3e53'}>
-
                 <DashHeader
                     pokemonId={pokemonId}
                     pokemonName={pokemonName}
@@ -107,41 +95,9 @@ const Dashboard: NextPage = () => {
                 />
 
                 <DashboardCards>
-                    <DashboardLeftCards>
-                        <PokemonImgCard>
-                            <Image src={pokemon.loading ? PokeballGif : pokemon.data.pokemon.sprites.front_default} width={370} height={370} />
-                        </PokemonImgCard>
-                        <PokemonTypeCard>
-                            <DashboardCardTitle>Type</DashboardCardTitle>
-                            {!pokemon.loading && pokemon.data.pokemon.types.map((type: any) =>
-                                <PokemonProperty key={Math.random()} className={type.type.name}>
-                                    {type.type.name}
-                                </PokemonProperty>
-                            )}
-                        </PokemonTypeCard>
-                        <DashboardMinorCard>
-                            <PokemonHWCard>
-                                <DashboardCardTitle>Height:</DashboardCardTitle>
-                                <DashboardSpan>
-                                    {!pokemon.loading && (pokemon.data.pokemon.height / 10) + ' m'}
-                                </DashboardSpan>
-                            </PokemonHWCard>
-                            <PokemonHWCard>
-                                <DashboardCardTitle>Weight:</DashboardCardTitle>
-                                <DashboardSpan>{!pokemon.loading && (pokemon.data.pokemon.weight / 10) + ' kg'}</DashboardSpan>
-                            </PokemonHWCard>
-                        </DashboardMinorCard>
-                        <PokemonAttributesCard>
-                            <DashboardCardTitle>Attributes</DashboardCardTitle>
-                            <PokemonAttributesGroup>
-                                {!pokemon.loading && pokemon.data.pokemon.stats.map((stat: any) =>
-                                    <PokemonProperty key={Math.random()} className="attr">
-                                        {stat.base_stat} {stat.stat.name}
-                                    </PokemonProperty>
-                                )}
-                            </PokemonAttributesGroup>
-                        </PokemonAttributesCard>
-                    </DashboardLeftCards>
+                    <DashLeftCards
+                        pokemon={pokemon}
+                    />
 
                     <DashboardRightCards>
                         <PokemonEvolutionCard>
